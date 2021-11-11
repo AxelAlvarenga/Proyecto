3157d1902d5442cb5d214e878df274ad51011509
AxelAlvarenga <axelalvarenga97@gmail.com>
2021-11-11 19:41:30 -0300

    pablo

--- servidor/pagina/urls.py
+++ servidor/pagina/urls.py
@@ -3,7 +3,7 @@ from pagina import views
 urlpatterns = [path('', views.login ,name='login'), 
 path('index.html', views.inicio ,name='inicio'),
 path('login.html', views.salir ,name='salir'),
-path('table-datatable.html', views.buscar ,name='buscar'),
+path('buscar', views.buscar ,name='buscar'),
 path('verproducto/<int:codproducto>', views.verproducto, name='verproducto'),
 
 
